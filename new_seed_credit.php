c7384146a27db42dd45a4aad65936432aa8efbf3
Aydin Aydin <Aydin@Aydins-MacBook-Air.local>
2016-10-04 12:04:40 +0200

    Major changes have been made, added new features in the backend.

--- new_seed_credit.php
+++ new_seed_credit.php
@@ -1,21 +1,61 @@
 <?php
 /*
- * Plugin Name: NewSeed Credit Widget
- * Description: Show the other plugins you are using on your wordpress site or blogg.
- * Plugin URI: http://newseed.se/
- * Version: 1.0
- * Author: Aydin Nalbantov
- * Author URI: http://www.aydin.cf/plugins/
- * License: GPLv2
- */
+* Plugin Name: NewSeed Credit Widget
+* Description: Show the other plugins you are using on your wordpress site or blogg.
+* Plugin URI: http://newseed.se/
+* Version: 1.0
+* Author: Aydin Nalbantov
+* Author URI: http://www.aydin.cf/plugins/
+* License: GPLv2 or later
+
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
 /* Start Adding Functions Below this Line */
+function nsc_admin_page() {
+  include_once 'plugin_options.php';
+  add_menu_page ( 'New Seed Settings', 'New Seed', 'manage_options', 'nsc_admin_menu', 'nsc_create_page', plugins_url ('new_seed/img/png_new.png'), 6 );
+  add_submenu_page ( 'nsc_admin_menu', 'New Seed Settings', 'Settings', 'manage_options', 'nsc_admin_menu', 'nsc_create_page' );
+  add_submenu_page ( 'nsc_admin_menu', 'New Seed Features', 'Features', 'manage_options', 'nsc_admin_menu_fordon', 'nsc_settings_page' );
+}
+add_action( 'admin_menu', 'nsc_admin_page');
+add_shortcode( 'newseed', 'displayPluginContent' );
+   function displayPluginContent() {
+    // Check if get_plugins() function exists. This is required on the front end of the
+    // site, since it is in a file that is normally only loaded in the admin.
+    if ( ! function_exists( 'get_plugins' ) ) {
+      require_once ABSPATH . 'wp-admin/includes/plugin.php';
+    }
+    $plugins = get_plugins();
+    $i = 0;
+    foreach ($plugins as $plugin){
+      if(get_option('plugin'.$i) != null){
+        //$plugin = get_option('plugin'.$i);
+        echo '<b>Name:</b>' . ' ' . $plugin['Name'];
+        echo '</br>';
+        echo '<b>URL:</b>' . ' ' . $plugin['PluginURI'];
+        echo '</br>';
+        echo '<b>Version</b>' . ' ' . $plugin['Version'];
+        echo '</br>' . '</br>';
+      }
+      $i++;
+    }
+  }
+  // Creating the widget
+  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+  class wpb_widget extends WP_Widget {
 
-// Creating the widget
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-class wpb_widget extends WP_Widget {
-
-  function __construct() {
-    parent::__construct(
+    function __construct() {
+      parent::__construct(
       // Base ID of the widget
       'wpb_widget',
 
@@ -55,9 +95,10 @@ class wpb_widget extends WP_Widget {
     foreach ($plugins as $plugin){
       if(isset($instance['plugin'.$i])){
         $plugin = $instance['plugin'.$i];
+        $checked = 'checked';
         echo '<b>Name:</b>' . ' ' . $plugin['Name'];
         echo '</br>';
-        echo '<b>URL:</b>' . ' ' . $plugin['PluginURI'];
+        echo '<b>URL:</b>' . ' <a href="' .$plugin['PluginURI'] . '">'.$plugin['PluginURI'].'</a>';
         echo '</br>';
         echo '<b>Version</b>' . ' ' . $plugin['Version'];
         echo '</br>' . '</br>';
@@ -67,69 +108,68 @@ class wpb_widget extends WP_Widget {
   }
 
   public function form( $instance ) {
-  $plugins = get_plugins();
-  $i = 0;
-  echo "<table>";
+    $plugins = get_plugins();
+    $i = 0;
+    echo "<table>";
 
-  foreach ($plugins as $plugin){
-    $checked="";
-    if(isset($instance['plugin'.$i])){
-      $checked = 'checked';
+    foreach ($plugins as $plugin){
+      $checked="";
+      if(isset($instance['plugin'.$i])){
+        $checked = 'checked';
+      }
+      echo "<tr><td>";
+      echo '<b style >Name:</b>' . ' ' . $plugin['Name'];
+      echo '</br>';
+      echo '<b>URL:</b>' . ' ' . $plugin['PluginURI'];
+      echo '</br>';
+      echo '<b>Version</b>' . ' ' . $plugin['Version'];
+      echo '</br></td><td>';
+      echo '<input class="widefat" id="'. $this->get_field_id( 'plugin'.$i ).'" name="'. $this->get_field_name( 'plugin'.$i ).'" type="checkbox" value= "' . $this->get_field_name( 'plugin'.$i ) .'" '. $checked .' />';
+      echo "</td></tr>";
+      $i++;
     }
-    echo "<tr><td>";
-    echo '<b style >Name:</b>' . ' ' . $plugin['Name'];
-    echo '</br>';
-    echo '<b>URL:</b>' . ' ' . $plugin['PluginURI'];
-    echo '</br>';
-    echo '<b>Version</b>' . ' ' . $plugin['Version'];
-    echo '</br></td><td>';
-    echo '<input class="widefat" id="'. $this->get_field_id( 'plugin'.$i ).'" name="'. $this->get_field_name( 'plugin'.$i ).'" type="checkbox" value= "' . $this->get_field_name( 'plugin'.$i ) .'" '. $checked .' />';
-    echo "</td></tr>";
-    $i++;
-  }
-  echo "</table>";
+    echo "</table>";
 
-  if ( isset( $instance[ 'title' ] ) ) {
-    $title = $instance[ 'title' ];
-  }
-  else {
-    $title = __( 'New title', 'wpb_widget_domain' );
-    $checkbox = '';
-  }
+    if ( isset( $instance[ 'title' ] ) ) {
+      $title = $instance[ 'title' ];
+    }
+    else {
+      $title = __( 'New title', 'wpb_widget_domain' );
+      $checkbox = '';
+    }
 
-// Widget admin form
-?>
-<p>
-<label form="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+    // Widget admin form
+    ?>
+    <p>
+      <label class="the-title"form="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+      <?php
+    }
 
-<?php
-}
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+      error_log( print_r( $new_instance, true ) );
+      $plugins = get_plugins();
+      $i = 0;
+      foreach ($plugins as $plugin){
+        if(isset($new_instance['plugin'.$i])){
+          $instance['plugin'.$i] = $plugin;
+        }
+        $i++;
+      }
+      $instance['textarea'] = strip_tags($new_instance['textarea']);
+      $instance['checkbox'] = strip_tags($new_instance['checkbox']);
+      $instance['select'] = strip_tags($new_instance['select']);
+      $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+      return $instance;
+    }
+  } // Class wpb_widget ends here
 
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-error_log( print_r( $new_instance, true ) );
-$plugins = get_plugins();
-$i = 0;
-foreach ($plugins as $plugin){
-  if(isset($new_instance['plugin'.$i])){
-    $instance['plugin'.$i] = $plugin;
+  // Register and load the widget
+  function wpb_load_widget() {
+    register_widget( 'wpb_widget' );
+   // wp_enqueue_style( 'style', plugins_url( 'css/style.css', __FILE__ ), false, 'v1', $media = 'all' );
   }
-  $i++;
-}
-  $instance['textarea'] = strip_tags($new_instance['textarea']);
-  $instance['checkbox'] = strip_tags($new_instance['checkbox']);
-  $instance['select'] = strip_tags($new_instance['select']);
-  $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-  return $instance;
-  }
-} // Class wpb_widget ends here
-
-// Register and load the widget
-function wpb_load_widget() {
-  register_widget( 'wpb_widget' );
-  wp_enqueue_style( 'style', plugins_url( 'css/style.css', __FILE__ ), false, 'v1', $media = 'all' );
-}
-add_action( 'widgets_init', 'wpb_load_widget' );
-/* Stop Adding Functions Below this Line */
-?>
+  add_action( 'widgets_init', 'wpb_load_widget' );
+  /* Stop Adding Functions Below this Line */
+ ?>
