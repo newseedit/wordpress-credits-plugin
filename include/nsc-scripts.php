d1ea24c2dec9edc1b50be0fcfd7e7d5553e0c480
Aydin Aydin <Aydin@Aydins-MacBook-Air.local>
2016-10-05 04:03:07 +0200

    Changed color on the frontpage plugin. removed some links.

--- include/nsc-scripts.php
+++ include/nsc-scripts.php
@@ -1,5 +1,3 @@
 <?php
 // Add Scripts
   wp_enqueue_style('style',plugins_url() . '/new_seed/css/style.css');
-  wp_enqueue_style('style',plugins_url() . '/new_seed/css/nsc-admin-style.css');
-  wp_enqueue_style('style',plugins_url() . '/new_seed/css/nsc-plugin.css');
